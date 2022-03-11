75e2be5c29804ca58a1ab5135214738b9e4fe945
23pras <palprashant465@gmail.com>
2022-03-11 18:17:17 +0530

    read and write file.

--- complete-node-bootcamp-master/1-node-farm/index.js
+++ complete-node-bootcamp-master/1-node-farm/index.js
@@ -1,4 +1,24 @@
-const fs = require('fs');
+const fs = require('fs');     // fs is a module which have so many functions which we will be using. 
 
-const hello = "Hello World";
-console.log(hello);
+// const hello = "Hello World";
+// console.log(hello);
+
+// This readFileSync func is reading a files data from a location.
+
+const textIn = fs.readFileSync('./starter/txt/input.txt' , 'utf-8');
+//console.log(textIn);
+
+
+var today = new Date();
+var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+var dateTime = date+' '+time;
+
+const textAdded = `this is about avocado : ${textIn}.\ncreated on ${dateTime}`;
+console.log(textAdded);
+
+// This write fun is creating a file at a location and adding the text into it.
+
+fs.writeFileSync('./starter/txt/output.txt', textAdded);
+console.log("File added with text");
+
