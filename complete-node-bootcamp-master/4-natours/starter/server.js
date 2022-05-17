1695d045417b5a8e68e85db20f13b3cd985aba63
23pras <palprashant465@gmail.com>
2022-05-17 17:58:31 +0530

    mongodb connected

--- complete-node-bootcamp-master/4-natours/starter/server.js
+++ complete-node-bootcamp-master/4-natours/starter/server.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
@@ -7,9 +8,21 @@ const app = require('./app');
 // environment variables are global variables that are used to define the env in which the node app is running, its set by express
 // console.log(process.env);
 
-// Server starts
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+//const DB = process.env.DATABASE;
 
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useCreateIndex : true,
+  useFindAndModify: false,
+  useUnifiedTopology: true 
+}).then( ()=>{
+  console.log('DB connection established')  
+});
+
+// Server starts
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App is running on port num ${port}... `);
 });
+
