8f453ff38411736f2a201bb4c5c8345364fca273
23pras <palprashant465@gmail.com>
2022-05-23 18:24:52 +0530

    first document

--- complete-node-bootcamp-master/4-natours/starter/server.js
+++ complete-node-bootcamp-master/4-natours/starter/server.js
@@ -38,6 +38,18 @@ const tourSchema = new mongoose.Schema({
 
 const Tour = mongoose.model('Tour', tourSchema);
 
+const testTour = new Tour({
+  name : "Prashant 23",
+  rating: 8.9,
+  price : 889
+});
+
+testTour.save().then(doc=>{
+  console.log(doc)
+}).catch(err=>{
+  console.log('Error :', err)
+});
+
 // Server starts
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
