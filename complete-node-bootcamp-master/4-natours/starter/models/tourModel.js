536de676f00d5897bdb509e204695a053e4ada35
23pras <palprashant465@gmail.com>
2023-06-24 18:26:42 +0530

    virtual prop

--- complete-node-bootcamp-master/4-natours/starter/models/tourModel.js
+++ complete-node-bootcamp-master/4-natours/starter/models/tourModel.js
@@ -52,8 +52,16 @@ const tourSchema = new mongoose.Schema({
       select: false
     },
     startDates : [Date]
+  },
+  {
+    toJSON : { virtuals: true},
+    toObject : {virtuals : true}
   });
   
+  tourSchema.virtual('durationWeeks').get(function(){
+    return this.duration/7
+  });
+
   const Tour = mongoose.model('Tour', tourSchema);
 
   module.exports = Tour;
