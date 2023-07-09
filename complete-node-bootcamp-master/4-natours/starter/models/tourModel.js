3f1e8c2d853013e233788c037049d9b315c43558
23pras <palprashant465@gmail.com>
2023-07-09 23:02:47 +0530

    docs middleware mongoose

--- complete-node-bootcamp-master/4-natours/starter/models/tourModel.js
+++ complete-node-bootcamp-master/4-natours/starter/models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema({
     name: {
@@ -7,6 +8,7 @@ const tourSchema = new mongoose.Schema({
       unique: true,
       trim: true
     },
+    slug: String,
     duration :{
       type: Number,
       required: [true, "A tour must have a duration"]
@@ -61,6 +63,18 @@ const tourSchema = new mongoose.Schema({
   tourSchema.virtual('durationWeeks').get(function(){
     return this.duration/7
   });
+
+  // document middleware : runs before .save() and .create()
+
+  tourSchema.pre('save', function(next){
+     this.slug = slugify(this.name , {lower:true});
+     next();
+  });
+
+  // tourSchema.post('save', function(doc,next){
+  //    console.log("line 75  >>>  ", doc);
+  //    next();
+  // });
 
   const Tour = mongoose.model('Tour', tourSchema);
 
